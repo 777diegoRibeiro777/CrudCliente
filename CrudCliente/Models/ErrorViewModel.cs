3da012a0512f4f9ea706e78fc3ebea816fdd725f
Diego Ribeiro <51891308+777diegoRibeiro777@users.noreply.github.com>
2024-02-21 20:45:59 -0300

    Adicionar arquivos de projeto.

---
+++ CrudCliente/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CrudCliente.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ CrudCliente/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿let table = new DataTable('#table_cliente', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
+    },
+});
+
+$(document).ready(function () {
+    $(selector).inputmask("99-9999999");  //static mask
+    $(selector).inputmask({ "mask": "(999) 999-9999" }); //specifying options
+    $(selector).inputmask("9-a{1,3}9{1,3}"); //mask with dynamic syntax
+});
